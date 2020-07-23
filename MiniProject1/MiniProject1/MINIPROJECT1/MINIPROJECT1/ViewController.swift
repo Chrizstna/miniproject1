a72dd7e585e0298c09d89c359480cdbbd8f451e9
Chrizstna <68654815+Chrizstna@users.noreply.github.com>
2020-07-23 00:51:52 -0400

    Add files via upload

---
+++ MiniProject1/MiniProject1/MINIPROJECT1/MINIPROJECT1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MINIPROJECT1
+//
+//  Created by Christina Pan on 7/23/20.
+//  Copyright © 2020 Christina Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buttonOne: UILabel!
+    @IBOutlet weak var buttonTwo: UILabel!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var imageOne: UIImageView!
+    @IBOutlet weak var imageTwo: UIImageView!
+    @IBOutlet weak var appTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
