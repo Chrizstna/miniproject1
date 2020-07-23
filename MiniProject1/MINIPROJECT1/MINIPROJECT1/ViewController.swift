4e982040f8ce56d41cb6b1b853637c8e7e1aed81
Chrizstna <68654815+Chrizstna@users.noreply.github.com>
2020-07-23 00:33:57 -0400

    Add files via upload

---
+++ MiniProject1/MINIPROJECT1/MINIPROJECT1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MINIPROJECT1
+//
+//  Created by Christina Pan on 7/23/20.
+//  Copyright © 2020 Christina Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
